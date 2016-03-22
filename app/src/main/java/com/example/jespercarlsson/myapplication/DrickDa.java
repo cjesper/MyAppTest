1af01732044619e7a858dd5d65fd8ed2b8868285
Jesper Carlsson <jespercarlsson@Jespers-MacBook-Air.local>
2016-03-22 16:07:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jespercarlsson/myapplication/DrickDa.java
@@ -0,0 +1,16 @@
+package com.example.jespercarlsson.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrickDa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drick_da);
+
+        System.out.println("DDJF");
+
+    }
+}
